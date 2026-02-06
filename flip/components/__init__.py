c25b084e655e9d2c99be7b92f51f3b9be992bbda
R. Garcia-Dias <rafaelagd@gmail.com>
2026-02-06 14:53:41 +0000

    feat: Add placeholder modules for components and controllers
    - Add empty components module for future NVFLARE components
    - Add empty controllers module for future FL controllers
    - Prepare package structure for extensibility

---
+++ flip/components/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) Guy's and St Thomas' NHS Foundation Trust & King's College London
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+FLIP Components module containing reusable FL components.
+
+Components include event handlers, model locators, and persistence utilities.
+
+Exports:
+    - ClientEventHandler: Client-side event handler
+    - ServerEventHandler: Server-side event handler
+    - PTModelLocator: PyTorch model locator
+    - ValidationJsonGenerator: Validation results JSON generator
+"""
+
+# Components will be imported here after extraction
+# from flip.components.event_handlers import ClientEventHandler, ServerEventHandler
+# from flip.components.model_locator import PTModelLocator
+# from flip.components.json_generators import ValidationJsonGenerator
+
+__all__ = []
---
+++ flip/controllers/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) Guy's and St Thomas' NHS Foundation Trust & King's College London
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+FLIP Controllers module containing NVFLARE workflow controllers.
+
+Controllers orchestrate federated learning workflows.
+
+Exports:
+    - InitTraining: Initialization controller for training setup
+    - ScatterAndGather: Main training loop controller with FedAvg aggregation
+    - CrossSiteModelEval: Cross-site model evaluation controller
+"""
+
+# Controllers will be imported here after extraction
+# from flip.controllers.init_training import InitTraining
+# from flip.controllers.scatter_and_gather import ScatterAndGather
+# from flip.controllers.cross_site_model_eval import CrossSiteModelEval
+
+__all__ = []
